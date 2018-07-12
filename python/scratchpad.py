6117a3b237ae4cc165ce430a4f99be2188d992f1
Sebastian Kurscheid <genomicsio@gmail.com>
2018-07-12 15:05:52 +1000

    see previous commit

--- python/scratchpad.py
+++ python/scratchpad.py
@@ -1,16 +1,39 @@
 from snakemake.io import expand, glob_wildcards
 import json
 from pprint import pprint
+import os
 
 REF_GENOME = config["references"]["genomes"][1]
 REF_VERSION = config["references"][REF_GENOME]["version"][2]
+
+chromSizes = home + config["references"][REF_GENOME]["chromSizesUCSC"],
+gaps = home + config["references"][REF_GENOME]["gapsUCSC"],
+binSize = "", # don't set for autoestimation
+numTrials = "10000", #number of Monte Carlo trials 10000 default
+fdr = "0.1",
+gapPenalty = "", # using default - autodetect
+eddBinDir = home + config["edd_dir"]
 
 wildcards = {"assayID" : "ChIP-Seq",
              "runID" : "run1",
              "outdir" : "processed_data",
              "reference_version": REF_VERSION,
              "application" : "bowtie2",
-             "sample" : "ASC-ad-D0-H3K9me2"}
+             "sample" : "ASC-ad-D0-H3K9me2",
+             "type" : "replicates"}
+
+bam = lambda wildcards: expand("{runID}/{outdir}/{reference_version}/bowtie2/{unit}.final.{suffix}",
+                         outdir = wildcards["outdir"],
+                         reference_version = wildcards["reference_version"],
+                         unit = config["samples"]["ChIP-Seq"][wildcards["type"]][wildcards["sample"]],
+                         suffix = ["bam"])
+
+expand("{runID}/{outdir}/{reference_version}/bowtie2/{unit}.final.{suffix}",
+        runID = wildcards["runID"],
+        outdir = wildcards["outdir"],
+        reference_version = wildcards["reference_version"],
+        unit = config["samples"]["ChIP-Seq"][wildcards["type"]][wildcards["sample"]],
+        suffix = ["bam"])
 
 expand("{runID}/{outdir}/{reference_version}/deepTools/plotFingerprint/fingerprints_duplicates_removed.png",
        runID = "run1",
@@ -25,3 +48,25 @@ expand("{runID}/{outdir}/{reference_version}/deepTools/plotCorrelation/heatmap_S
         outdir = config["processed_dir"],
         reference_version = REF_VERSION,
         suffix = ["png", "tab"])
+
+expand("{runID}/{outdir}/{reference_version}/deepTools/bamCompare/{treatment}_vs_{control}.bw",
+       runID = "run1",
+       outdir = config["processed_dir"],
+       reference_version = REF_VERSION,
+       treatment = SAMPLES[0:2],
+       control = SAMPLES[2])
+
+               control = SAMPLES[2]),
+expand("{runID}/{outdir}/{reference_version}/deepTools/bamCompare/{treatment}_vs_{control}.bw",
+       runID = "run1",
+       outdir = config["processed_dir"],
+       reference_version = REF_VERSION,
+       treatment = SAMPLES[3:5],
+       control = SAMPLES[5])
+
+expand("{runID}/{outdir}/{reference_version}/deepTools/bamCompare/{treatment}_vs_{control}.bw",
+       runID = "run1",
+       outdir = config["processed_dir"],
+       reference_version = REF_VERSION,
+       treatment = SAMPLES[6:8],
+       control = SAMPLES[8])
--- snakemake/rules/run_edd.py
+++ snakemake/rules/run_edd.py
@@ -12,6 +12,12 @@ __date__ = "2018-07-11"
 """
 
 rule run_edd:
+    params:
+        chromSizes = home + config["references"][REF_GENOME]["chromSizesUCSC"],
+        gaps = home + config["references"][REF_GENOME]["gapsUCSC"],
+        numTrials = "10000",
+        fdr = "0.1",
+        eddBinDir = home + config["edd_dir"]
     input:
         control = "{runID}/{outdir}/{reference_version}/bowtie2/merged/{Input}.bam",
         chip = "{runID}/{outdir}/{reference_version}/bowtie2/merged/{ChIP}.bam"
@@ -21,18 +27,5 @@ rule run_edd:
         "run_edd_{ChIP}_vs_{Input}.log"
     threads:
         8
-    params:
-        chromSizes = home + config["references"][REF_GENOME]["chromSizesUCSC"],
-        gaps = home + config["references"][REF_GENOME]["gapsUCSC"],
-        numTrials = "10000",
-        fdr = "0.1",
-        eddBinDir = home + config["edd_dir"]
     shell:
-        """"
-            {params.eddBinDir}/edd {params.chromSizes} {params.gaps} {input.chip} {input.control} {output}\
-                                   -n {params.numTrials}\
-                                   --fdr {params.fdr}\
-                                   --nprocs {threads}\
-                                   --write-log-ratios\
-                                   --write-bin-scores 1>>{log} 2>>{log}\
-        """"
+        "{params.eddBinDir}/edd {params.chromSizes} {params.gaps} {input.chip} {input.control} {output} -n {params.numTrials} --fdr {params.fdr} --nprocs {threads} --write-log-ratios --write-bin-scores 1>>{log} 2>>{log}"
